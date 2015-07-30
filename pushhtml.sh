4afb892e05ae76dc85815f9dd510de165514606f
sin <sinoory@126.com>
2015-07-30 19:13:58 +0800

    bugfix in html

--- pushhtml.sh
+++ pushhtml.sh
@@ -1,6 +1,16 @@
 
-adb push assets/html/userlist.html /sdcard/sin/gps/html/
-adb push assets/html/linelist.html /sdcard/sin/gps/html/
-adb push assets/html/serverbuslist.html /sdcard/sin/gps/html/
 
-adb push assets/js/jquery_mini.js /sdcard/sin/gps/js/
+if [ -z "$1"  ] ; then
+    devs=""
+elif [ "$1" = "e" ] ; then
+    devs=" -s emulator-5554 "
+else
+    devs=" -s 43c720140df62d7 "
+fi
+echo "$devs"
+adb $devs push assets/html/userlist.html /sdcard/sin/gps/html/
+adb $devs push assets/html/linelist.html /sdcard/sin/gps/html/
+adb $devs push assets/html/serverbuslist.html /sdcard/sin/gps/html/
+
+adb $devs push assets/js/jquery_mini.js /sdcard/sin/gps/js/
+
