ce861fbdceb2772dfee207b7b18bbef202e74f93
sin <sinoory@126.com>
2015-09-09 20:00:48 +0800

    move runningbus to app/view/Runningbus.js ok

---
+++ assets/html/apps/runbus/app/view/Runningbus.js
@@ -0,0 +1,40 @@
+Ext.define('RunBus.view.Runningbus',{
+    extend:'Ext.Container',
+    config: {
+        fullscreen: true,
+        layout:'vbox', //TODO:impotant , otherwise the list can'b show
+        items: [
+            {
+                xtype: 'titlebar',title:'BUS',id:'idbusTitle',
+            },
+            {
+                xtype: 'container',flex:1,id:'idbus',
+                style : "border-style: solid;",
+                scrollable: {direction: 'vertical',directionLock: true},
+            },
+            {
+                xtype: 'titlebar',title: 'bus infomations',titleAlign:'left',id:'idbusinfo',
+                items:[
+                    {xtype:'button',html:'-',ui:'action',align:'right',id:'idshowbus',},
+                ],
+            },
+            {
+                xtype: 'container',id:'idchat',flex:1,
+                style : "border-style: solid;",
+                scrollable: {direction: 'vertical',directionLock: true},
+            },
+            {
+                xtype: 'container',
+                layout: 'hbox',
+                style : "border-style: solid;border-radius: 1px",
+                items: [
+                    {xtype: 'textfield',name : 'name',flex:5,id:'idmsg'},
+                    {xtype:'button',html:'Send',ui: 'action',flex:1,id:'idsend',},
+                ],    
+
+            }
+
+
+        ],
+    },
+});
--- assets/html/apps/runbus/contain.js
+++ assets/html/apps/runbus/contain.js
@@ -3,49 +3,10 @@ Ext.application({
     name: 'RunBus',
     //
     //controllers: ['MainControl'],
-    //controllers: ['TestMain'],
-    views:['LocalList'],
+    controllers: ['TestMain'],
+    views:['LocalList','Runningbus'],
     launch: function() {   
-        Ext.create('RunBus.view.LocalList');
-        //Ext.Viewport.add({xclass:'RunBus.view.Buttons'});
-        //Ext.Viewport.add({xclass:'RunBus.view.LocalList'});
-        /*
-        var senderroot = Ext.create('Ext.Container', {
-            layout: 'hbox',
-            style : "border-style: solid;border-radius: 1px",
-                items: [
-                    {xtype: 'textfield',name : 'name',flex:5,id:'idmsg'},
-                    {xtype:'button',html:'Send',ui: 'action',flex:1,id:'idsend',},
-                ],    
-            });
-
-        var mainui=Ext.create('Ext.Container', {
-            fullscreen: true,layout: 'vbox',id:'idmain',
-            items: [
-                {
-                    xtype: 'titlebar',title:'BUS',id:'idbusTitle',
-                },
-                {
-                    xtype: 'container',flex:1,id:'idbus',
-                    style : "border-style: solid;",
-                    scrollable: {direction: 'vertical',directionLock: true},
-                },
-                {
-                    xtype: 'titlebar',title: 'bus infomations',titleAlign:'left',id:'idbusinfo',
-                    items:[
-                        {xtype:'button',html:'-',ui:'action',align:'right',id:'idshowbus',},
-                    ],
-                },
-                {
-                    xtype: 'container',id:'idchat',flex:1,
-                    style : "border-style: solid;",
-                    scrollable: {direction: 'vertical',directionLock: true},
-                }
-            ]
-        });
-        chartroot=Ext.ComponentQuery.query("#idchat")[0];
-        busroot=Ext.ComponentQuery.query("#idbus")[0];
-        mainui.add(senderroot);
-*/
+        //Ext.create('RunBus.view.LocalList');
+        Ext.create('RunBus.view.Runningbus');
     },
 });
